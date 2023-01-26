2f0bf34c6cf07f3338d15c679d5951518a18a1a2
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-26 15:50:42 +1300

    finished makeGrid function to make 16x16 grid

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const container = document.querySelector(".container");
+container.style.width = "640px"
+container.style.height = "640px"
 
 makeGrid = () => {
-    for(let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
+    for(let i = 0; i < 16; i++) {
+        for (let j = 0; j < 16; j++) {
             let div = document.createElement("div");
             div.style.width = "40px";
             div.style.height = "40px";
-            div.style.backgroundColor = "grey";
-            div.classList.add = "box"
+            div.style.background = "white";
+            div.style.outline = "1px";
+            div.style.outlineColor = "black";
+            div.style.outlineStyle = "solid";
+            div.className = "box";
             container.appendChild(div);
         }
     }
