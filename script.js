6455b0c54080bda5bcbb50e8085b4b1d41123ebb
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-26 14:30:06 +1300

    Added style to divs and container dimensions

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ makeGrid = () => {
     for(let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             let div = document.createElement("div");
-            div.style.width = "10px";
-            div.style.height = "10px";
+            div.style.width = "40px";
+            div.style.height = "40px";
+            div.style.backgroundColor = "grey";
+            div.classList.add = "box"
             container.appendChild(div);
         }
     }
